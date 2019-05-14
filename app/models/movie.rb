e9579eb45d030a822612f058b682cc3811fd7b9c
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-14 14:38:04 -0700

    changed inventory validation to be greater than -1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   has_and_belongs_to_many :customers
   validates :title, presence: true
-  validates :inventory, presence: true, numericality: {greater_than: 0}
+  validates :inventory, presence: true, numericality: {greater_than: -1}
 end
