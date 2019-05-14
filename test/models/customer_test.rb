68236e318b6844d1515309e75fe2844a6a81a069
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-14 14:55:30 -0700

    wrote validation tests for customer model

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,27 @@
 require_relative "../test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { customers(:myriam) }
 
   it "must be valid" do
     value(customer).must_be :valid?
+  end
+
+  it "will save with a name" do
+    new_cust = Customer.new(name: "Sarah")
+
+    expect {
+      new_cust.save
+    }.must_change "Customer.count", 1
+  end
+
+  it "won't save without a name" do
+    new_cust = Customer.new
+
+    expect {
+      new_cust.save
+    }.wont_change "Customer.count"
+
+    expect(new_cust.errors.messages).must_include :name
   end
 end
