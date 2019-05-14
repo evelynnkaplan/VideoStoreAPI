e19ceea417f2945f0de6bc4f64db645c26f5fe8b
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-14 14:48:01 -0700

    validation tests for movie model

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:movie) { movies(:lucky) }
 
   it "must be valid" do
     value(movie).must_be :valid?
+  end
+
+  it "will save with a title and inventory" do
+    new_movie = Movie.new(title: "Lucky", inventory: 11)
+
+    expect {
+      new_movie.save
+    }.must_change "Movie.count", 1
+  end
+
+  it "won't save without a title" do
+    new_movie = Movie.new(inventory: 11)
+
+    expect {
+      new_movie.save
+    }.wont_change "Movie.count"
+  end
+
+  it "won't save without an inventory" do
+    new_movie = Movie.new(title: "Lucky")
+
+    expect {
+      new_movie.save
+    }.wont_change "Movie.count"
   end
 end
