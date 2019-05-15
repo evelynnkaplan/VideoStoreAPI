e823963e30900410853182139cc39bd117cc3e03
MyriamWD <myriam.waldend@gmail.com>
2019-05-15 13:48:59 -0700

    create show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,16 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
   end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if !movie.nil?
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory]), status: :ok
+    else
+      render json: {ok: false, message: ["Movie not found"]}, status: :not_found
+    end
+  end
+
+  def movie_params
+  end
 end
