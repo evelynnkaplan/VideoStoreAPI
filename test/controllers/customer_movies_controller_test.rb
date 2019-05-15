1f374b8b54f47ed9c83bc20e502e875db987ee72
MyriamWD <myriam.waldend@gmail.com>
2019-05-15 15:29:13 -0700

    test for checkout method in customer movies controller

---
+++ test/controllers/customer_movies_controller_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+describe CustomerMoviesController do
+  # it "must be a real test" do
+  #   flunk "Need real tests"
+  # end
+
+  describe "checkout" do
+    it "creates a new instance of customer movie" do
+      # increases count of CustomerMovies
+      # decrease inventory of associated movie
+      # set's checkout date to today
+      # calculates due date to 7 days from today - model method
+
+      customer = customers(:evelynn)
+      movie = movies(:lion_king)
+      movie_inventory = movie.inventory
+      checkout_data = {
+        customer: customer,
+        movie: movie,
+      }
+      expect { post checkout_path, params: {checkout: checkout_data} }.must_change "CustomerMovie.count", 1
+      expect(movie.inventory).must_equal movie_inventory - 1
+      expect(CustomerMovie.last.checkout_date).must_equal Date.today
+      expect(CustomerMovie.last.due_date).must_equal Date.today.next_week
+    end
+
+    it "returns an error for invalid data" do
+      checkout_data = {
+        customer: customer,
+      }
+      expect { post checkout_path, params: {checkout: checkout_data} }.wont_change "CustomerMovie.count"
+      expect(body).must_be_kind_of Hash
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "movie"
+      must_respond_with :bad_request
+    end
+  end
+end
