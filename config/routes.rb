032d96e6a804eff8831f8d84b6380a819d9fee33
MyriamWD <myriam.waldend@gmail.com>
2019-05-15 15:58:39 -0700

    added route for checkout method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :customers, except: [:edit, :new]
   resources :movies, except: [:edit, :new]
+  post "/rentals/check-out", to: "customer_movies#checkout", as: "checkout"
 end
