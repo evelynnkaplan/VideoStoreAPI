375ec6afc80ffd723573c7df6e432cbb0eba8bb2
MyriamWD <myriam.waldend@gmail.com>
2019-05-15 13:49:29 -0700

    tests for movie show action

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -35,4 +35,41 @@ describe MoviesController do
       end
     end
   end
+
+  describe "show" do
+    it "is a working route and returns JSON" do
+      movie = movies(:lucky)
+      get movie_path(movie)
+
+      expect(response.header["Content-Type"]).must_include "json"
+      must_respond_with :success
+    end
+
+    it "returns a hash" do
+      movie = movies(:lucky)
+      get movie_path(movie)
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+    end
+
+    it "returns an error when looking for a non existing movie" do
+      get movie_path(-1)
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+
+      must_respond_with :not_found
+    end
+
+    it "gets a movie with all the fields in the body" do
+      keys = ["id", "title", "overview", "release_date", "inventory"].sort
+
+      movie = movies(:lucky)
+      get movie_path(movie)
+
+      body = JSON.parse(response.body)
+      body.keys.sort.must_equal keys
+    end
+  end
 end
