23327640cc489aec0667a61dc49b5522f04e8f2f
MyriamWD <myriam.waldend@gmail.com>
2019-05-16 10:23:57 -0700

    tests for checkin action

--- test/controllers/customer_movies_controller_test.rb
+++ test/controllers/customer_movies_controller_test.rb
@@ -1,17 +1,8 @@
 require "test_helper"
 
 describe CustomerMoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
-
   describe "checkout" do
     it "creates a new instance of customer movie" do
-      # increases count of CustomerMovies
-      # decrease inventory of associated movie
-      # set's checkout date to today
-      # calculates due date to 7 days from today - model method
-
       customer = customers(:evelynn)
       movie = Movie.create(title: "Wow", overview: "Amazing!", release_date: "2019-05-14", inventory: 5)
       movie_inventory = movie.inventory
@@ -36,6 +27,49 @@ describe CustomerMoviesController do
       }
 
       expect { post checkout_path, params: {customer_movie: checkout_data} }.wont_change "CustomerMovie.count"
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "movie"
+      must_respond_with :bad_request
+    end
+  end
+
+  describe "check-in" do
+    it "find the customer movie entry and return the inventory to the movie" do
+      customer = customers(:evelynn)
+      movie = Movie.create(title: "Wow", overview: "Amazing!", release_date: "2019-05-14", inventory: 5)
+      movie_inventory = movie.inventory
+      checkout_data = {
+        customer_id: customer.id,
+        movie_id: movie.id,
+      }
+
+      post checkout_path, params: {customer_movie: checkout_data}
+
+      after_checkout = Movie.find_by(id: CustomerMovie.last.movie.id)
+
+      checkin_data = {
+        customer_id: customer.id,
+        movie_id: movie.id,
+      }
+
+      post checkin_path, params: {customer_movie: checkin_data}
+
+      after_checkin = Movie.find_by(id: CustomerMovie.last.movie.id)
+
+      expect(after_checkin.inventory).must_equal after_checkout.inventory + 1
+      expect(CustomerMovie.last.status).must_equal "returned"
+    end
+
+    it "returns an error for invalid data at checkin" do
+      customer = customers(:evelynn)
+      checkin_data = {
+        customer: customer,
+      }
+
+      post checkin_path, params: {customer_movie: checkin_data}
 
       body = JSON.parse(response.body)
       expect(body).must_be_kind_of Hash
