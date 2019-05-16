9791d3cb6b79748d963201affde787ad17317ac9
MyriamWD <myriam.waldend@gmail.com>
2019-05-16 10:24:39 -0700

    checkin route'

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :customers, except: [:edit, :new]
   resources :movies, except: [:edit, :new]
   post "/rentals/check-out", to: "customer_movies#checkout", as: "checkout"
+  post "/rentals/check-in", to: "customer_movies#checkin", as: "checkin"
 end
