720dc0051c4ceda9516204e604fb27ad37f1b8cc
MyriamWD <myriam.waldend@gmail.com>
2019-05-17 15:05:47 -0700

    add checkin/checkout methods to models

--- app/controllers/customer_movies_controller.rb
+++ app/controllers/customer_movies_controller.rb
@@ -21,34 +21,6 @@ class CustomerMoviesController < ApplicationController
       render json: {ok: false, errors: ["Customer has not checked out this movie"]},
              status: :bad_request
     end
-
-    #     customer_movie = CustomerMovie.new(customer_movie_params)
-    #     if customer_movie.save
-    #       checkout_movie = customer_movie.movie.title
-    #       customer_movie.checkout_date = Date.today
-    #       customer_movie.due_date = Date.today.next_week
-    #       customer_movie.movie.inventory -= 1
-    #       customer_movie.save
-    #       customer_movie.movie.save
-
-    #       render json: {movie: checkout_movie, checkout_date: customer_movie.checkout_date, due_date: customer_movie.due_date},
-    #              status: :ok
-    #     else
-    #       render json: {ok: false, errors: customer_movie.errors.messages},
-    #              status: :bad_request
-    #     end
-    #   end
-
-    #   def checkin
-    #     rental = CustomerMovie.find_rentals(customer_movie_params)
-
-    #     if rental != nil
-    #       render json: {movie: rental.movie.title, checkin_status: rental.status},
-    #              status: :ok
-    #     else
-    #       render json: {ok: false, errors: ["Customer has not checked out this movie"]},
-    #              status: :bad_request
-    #     end
   end
 
   private
--- test/models/customer_movie_test.rb
+++ test/models/customer_movie_test.rb
@@ -42,4 +42,74 @@ describe CustomerMovie do
       expect(relationship.errors.messages).must_include :movie
     end
   end
+
+  describe "checkout_movie" do
+    it "checks out a movie with good data" do
+      movie = Movie.create(title: "Wow", overview: "Amazing!", release_date: "2019-05-14", inventory: 5)
+      movie_inventory = movie.inventory
+
+      CustomerMovie.checkout_movie({
+        customer: customers(:evelynn),
+        movie: movie,
+      })
+
+      after_checkout = Movie.find_by(id: CustomerMovie.last.movie.id)
+
+      expect(after_checkout.inventory).must_equal movie_inventory - 1
+      expect(CustomerMovie.last.checkout_date).must_equal Date.today
+      expect(CustomerMovie.last.due_date).must_equal Date.today.next_week
+    end
+
+    it "returns nil if given bad data" do
+      customer = customers(:evelynn)
+
+      rental = CustomerMovie.checkout_movie({
+        customer: customer,
+      })
+
+      expect(rental).must_equal nil
+    end
+  end
+
+  describe "find_rentals" do
+    it "will check in a movie given good data" do
+      customer = customers(:evelynn)
+      movie = Movie.create(title: "Wow", overview: "Amazing!", release_date: "2019-05-14", inventory: 5)
+      movie_inventory = movie.inventory
+      checkout_data = {
+        customer_id: customer.id,
+        movie_id: movie.id,
+      }
+
+      CustomerMovie.checkout_movie({
+        customer: customers(:evelynn),
+        movie: movie,
+      })
+
+      after_checkout = Movie.find_by(id: CustomerMovie.last.movie.id)
+
+      checkin_data = {
+        customer_id: customer.id,
+        movie_id: movie.id,
+      }
+
+      CustomerMovie.find_rentals(checkin_data)
+
+      after_checkin = Movie.find_by(id: CustomerMovie.last.movie.id)
+
+      expect(after_checkin.inventory).must_equal after_checkout.inventory + 1
+      expect(CustomerMovie.last.status).must_equal "returned"
+    end
+
+    it "will return nil if given bad data" do
+      customer = customers(:evelynn)
+      checkin_data = {
+        customer: customer,
+      }
+
+      after_checkin = CustomerMovie.find_rentals(checkin_data)
+
+      expect(after_checkin).must_equal nil
+    end
+  end
 end
